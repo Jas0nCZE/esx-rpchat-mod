157cc285306083add9a42318b315b47a5c1e7d58
Xios_Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-01-21 22:09:18 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,32 @@
 Config = {}
 
 Config.Locale = 'en'
-Config.OnlyFirstname = false -- Use first name only // Usar solo en primer nombre
+Config.OnlyFirstname    = false -- Use first name only // Usar solo en primer nombre
 Config.EnableESXIdentity = true -- Usar nombres del juego (esx_identity) // Usar nombres reales
 
-Config.Tnotify = true
-Config.ESX = false
+Config.Distance         = 20 -- /do and /me distance / distancia del /me y /do
+
+--COMMANDS
+Config.PoliceCommand    = 'police'
+Config.AmbulanceCommand = 'ambulance'
+Config.LSCCommand       = 'lsc'
+Config.BennysCommand    = 'bennys'
+Config.TDNCommand       = 'tdn'
+Config.TaxiCommand      = 'taxi'
+
+Config.HelpCommand      = 'a'
+Config.IDCommand        = 'id'
+Config.meCommand        = 'me'
+Config.doCommand        = 'do'
+    
+--JOBS
+Config.PoliceJob        = 'police'
+Config.AmbulanceJob     = 'ambulance'
+Config.LSCJob           = 'mechanic'
+Config.BennysJob        = 'bennys'
+Config.TDNJob           = 'mecano'
+Config.TaxiJob          = 'taxi'
+
+--NOTIFICATION
+Config.Tnotify          = false
+Config.ESX              = true
