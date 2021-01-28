6cb12432f4fb50b9901cc93bd280a5874f499622
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-01-28 17:16:12 -0500

    die

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,12 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+	Citizen.Wait(0)
+    end
+end)
+
 RegisterNetEvent('esx_rpchat:sendProximityMessage')
 AddEventHandler('esx_rpchat:sendProximityMessage', function(playerId, title, message, color)
 	local player = PlayerId()
@@ -24,7 +33,8 @@ Citizen.CreateThread(function()
 	TriggerEvent('chat:addSuggestion', '/lsc',   _U('lsc_help'),   {{name = _U('generic_argument_name'), help = _U('generic_argument_help')}})	
 	TriggerEvent('chat:addSuggestion', '/tdn',   _U('tdn_help'),   {{name = _U('generic_argument_name'), help = _U('generic_argument_help')}})	
 	TriggerEvent('chat:addSuggestion', '/taxi',   _U('taxi_help'),   {{name = _U('generic_argument_name'), help = _U('generic_argument_help')}})
-	TriggerEvent('chat:addSuggestion', '/sheriff',   _U('sheriff_help'),   {{name = _U('generic_argument_name'), help = _U('generic_argument_help')}})	
+	TriggerEvent('chat:addSuggestion', '/sheriff',   _U('sheriff_help'),   {{name = _U('generic_argument_name'), help = _U('generic_argument_help')}})
+	TriggerEvent('chat:addSuggestion', '/dado',   _U('dado_help'),   {{name = _U('generic_argument_name'), help = _U('generic_argument_help')}})	
 end)
 
 AddEventHandler('onResourceStop', function(resource)
@@ -40,7 +50,32 @@ AddEventHandler('onResourceStop', function(resource)
 		TriggerEvent('chat:removeSuggestion', '/tdn')
 		TriggerEvent('chat:removeSuggestion', '/taxi')
 		TriggerEvent('chat:removeSuggestion', '/sheriff')
+		TriggerEvent('chat:removeSuggestion', '/dado')
 	end
 end)
 
-
+RegisterCommand(Config.DieCommand, function(id, args, rawCommand)
+	local id = GetPlayerServerId(NetworkGetEntityOwner(PlayerPedId()))
+	local ped = PlayerPedId()
+	if id == 0 then
+		print(_U('no') )
+	elseif IsPedInAnyVehicle(ped, false) then
+		if Config.Tnotify then
+			exports['t-notify']:Alert({
+				style  =  'error',
+				message  =  _U('no_dado')
+			})
+		elseif Config.ESX then
+			ESX.ShowAdvancedNotification('Admin', GetPlayerName(NetworkGetEntityOwner(PlayerPedId())), _U('no_dado'), 'CHAR_BLOCKED', 2, false, false, 130)
+		end
+	else
+		RequestAnimDict("anim@mp_player_intcelebrationmale@wank")
+		while not HasAnimDictLoaded("anim@mp_player_intcelebrationmale@wank") do
+			Citizen.Wait(1)
+		end
+		TaskPlayAnim(ped, "anim@mp_player_intcelebrationmale@wank", "wank", -25.0, -8.0, -1, 1, 0, false, false, false)
+			Citizen.Wait(1725)
+	 	ClearPedTasks(ped)
+		TriggerEvent('esx_rpchat:sendProximityMessage', id, _U('dado_prefix', id), math.random(1, 6), {255, 156, 43})
+	end
+end, false)
